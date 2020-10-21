3db297e2cb0181707db207240e1ebd0e02a2fee2
pedronazarito98 <pedronazarito@gmail.com>
2020-10-21 15:42:08 -0300

    21-10(2)

--- src/components/Home/sectionHome.js
+++ src/components/Home/sectionHome.js
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const SectionHome = () =>{
-    return(
-        <React.Fragment>
-        <section className='home'>
-            <header className='section-indent text-center'>
-                <h1 className='FirstTitle'>Naza Música em movimento</h1>
-                <hr className='linhaTexto'/>
-                <p className=' TextoCentral'>
-                    A META DA NAZA MUSIC É MAXIMIZAR O POTENCIAL DE CADA CLIENTE 
-                    E CONTRIBUIR PARA QUE O RETORNO DO TRABALHO SEJA LUCRATIVO.
-                </p>
-            </header>
-            <div className='SectionTitle'>
-                <h1>news</h1>
-                <hr className='linhaText'/>
-            </div>
-        </section>
-        </React.Fragment>
-    )
-}
+const SectionHome = () => {
+  return (
+    <React.Fragment>
+      <section className="home">
+        <header className="section-indent text-center">
+          <h1 className="FirstTitle">Naza Música em movimento</h1>
+          <hr className="linhaTexto" />
+          <p className=" TextoCentral">
+            A META DA NAZA MUSIC É MAXIMIZAR O POTENCIAL DE CADA CLIENTE E
+            CONTRIBUIR PARA QUE O RETORNO DO TRABALHO SEJA LUCRATIVO.
+          </p>
+        </header>
+        <div className="SectionTitle">
+          <h1>news</h1>
+          <hr className="linhaText" />
+        </div>
+      </section>
+    </React.Fragment>
+  );
+};
 
-export default SectionHome
+export default SectionHome;
