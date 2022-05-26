84e37f0065e145a1cbb288595f8752c911c3e181
pedro.nazarito <pedro.nazarito@cinnecta.com>
2022-05-26 13:38:02 -0300

    build

---
+++ build/precache-manifest.61f537828a7542da94c80ee355d22d24.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d4d18d4a9a47ebb8e886e3d4081d63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29479a904972aaaec12d",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "42bc9bc620808f628f99",
+    "url": "/static/css/main.e48bd9ce.chunk.css"
+  },
+  {
+    "revision": "29479a904972aaaec12d",
+    "url": "/static/js/2.d3e9e41c.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.d3e9e41c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42bc9bc620808f628f99",
+    "url": "/static/js/main.a76ed799.chunk.js"
+  },
+  {
+    "revision": "e46d28c113c1cd14ebe0",
+    "url": "/static/js/runtime-main.be3e2adc.js"
+  },
+  {
+    "revision": "444130cc95af790b3b506e87f272b619",
+    "url": "/static/media/Album-George.444130cc.jpeg"
+  },
+  {
+    "revision": "f2b671ca16bc9ef79af6ccf14b473e82",
+    "url": "/static/media/Album-Murilo.f2b671ca.jpeg"
+  },
+  {
+    "revision": "629e6ae6b5e01922dd181e7f40931109",
+    "url": "/static/media/Gasolina-Final.629e6ae6.png"
+  },
+  {
+    "revision": "daba65b93affcd2a0bdce2fe584df203",
+    "url": "/static/media/LuToledo-capa.daba65b9.png"
+  },
+  {
+    "revision": "4b022b8a413bd260966e50a74aa67904",
+    "url": "/static/media/NomeAmor-Tadeu.4b022b8a.jpeg"
+  },
+  {
+    "revision": "c54980b98971c28c0c33dad78fe3359d",
+    "url": "/static/media/Os-homens-de-Pijamas.c54980b9.png"
+  },
+  {
+    "revision": "22b4228d55b0d65be814d8fcbcf56c4d",
+    "url": "/static/media/PopRoca-Tadeu.22b4228d.jpeg"
+  },
+  {
+    "revision": "a2ae0d1503774011992015345b78b5d7",
+    "url": "/static/media/Tadeu-franco.a2ae0d15.jpg"
+  },
+  {
+    "revision": "6369775124ced8d24db4470cbdd938f5",
+    "url": "/static/media/TropicalRomance.63697751.jpeg"
+  },
+  {
+    "revision": "f1166eab2f5d22a8a9c65f9d13427b34",
+    "url": "/static/media/assessoria.f1166eab.png"
+  },
+  {
+    "revision": "e2871d2b63300afd321972b0ad218098",
+    "url": "/static/media/desenvolvimento.e2871d2b.png"
+  },
+  {
+    "revision": "2a40238b9e0f8d5feb2d099660f7517f",
+    "url": "/static/media/distribui.2a40238b.png"
+  },
+  {
+    "revision": "2edbac05972ffff296b030c18e6faeb2",
+    "url": "/static/media/divulga.2edbac05.png"
+  },
+  {
+    "revision": "b46ffe0d1f84cb02f880848595cae6df",
+    "url": "/static/media/hans-banner.b46ffe0d.png"
+  },
+  {
+    "revision": "7446e559a97e0c9e0a4cec0b57ba3632",
+    "url": "/static/media/hans-card.7446e559.jpg"
+  },
+  {
+    "revision": "fe88e522361b3c4d53ce765c0ecf989a",
+    "url": "/static/media/img-george.fe88e522.png"
+  },
+  {
+    "revision": "7948c677241b9579a39ad3b17c9e9a37",
+    "url": "/static/media/img-sobre.7948c677.jpg"
+  },
+  {
+    "revision": "7948c677241b9579a39ad3b17c9e9a37",
+    "url": "/static/media/img.7948c677.jpg"
+  },
+  {
+    "revision": "5c66bdfc164cd78c9778c03fd80a69d9",
+    "url": "/static/media/marketing.5c66bdfc.png"
+  },
+  {
+    "revision": "44c3d6d6511be15728adb74b1c704363",
+    "url": "/static/media/murilo-card.44c3d6d6.jpg"
+  },
+  {
+    "revision": "b28396865393d2e20f9bbcba4714b83a",
+    "url": "/static/media/murilo2.b2839686.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9ef06992c4610d8005312294252f367b.js"
+  "/precache-manifest.61f537828a7542da94c80ee355d22d24.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
