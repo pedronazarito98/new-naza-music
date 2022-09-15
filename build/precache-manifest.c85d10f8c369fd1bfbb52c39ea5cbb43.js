019834b383eae77e867f8acc88caecccbc99a125
pedro.nazarito <pedro.nazarito@cinnecta.com>
2022-09-15 11:29:48 -0300

    build

---
+++ build/precache-manifest.c85d10f8c369fd1bfbb52c39ea5cbb43.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fcfd4ee470312c0427392504b6075cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42cbbb82b4fdde3eb56a",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "23b3930e8b94f57394ed",
+    "url": "/static/css/main.c4e94099.chunk.css"
+  },
+  {
+    "revision": "42cbbb82b4fdde3eb56a",
+    "url": "/static/js/2.b40cbd44.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.b40cbd44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23b3930e8b94f57394ed",
+    "url": "/static/js/main.25eeb2f1.chunk.js"
+  },
+  {
+    "revision": "e46d28c113c1cd14ebe0",
+    "url": "/static/js/runtime-main.be3e2adc.js"
+  },
+  {
+    "revision": "444130cc95af790b3b506e87f272b619",
+    "url": "/static/media/Album-George.444130cc.jpeg"
+  },
+  {
+    "revision": "f2b671ca16bc9ef79af6ccf14b473e82",
+    "url": "/static/media/Album-Murilo.f2b671ca.jpeg"
+  },
+  {
+    "revision": "4b022b8a413bd260966e50a74aa67904",
+    "url": "/static/media/NomeAmor-Tadeu.4b022b8a.jpeg"
+  },
+  {
+    "revision": "22b4228d55b0d65be814d8fcbcf56c4d",
+    "url": "/static/media/PopRoca-Tadeu.22b4228d.jpeg"
+  },
+  {
+    "revision": "1539c038e0fb566e5381eb058a5e5db4",
+    "url": "/static/media/StephanieJacovine-capa.1539c038.png"
+  },
+  {
+    "revision": "05a0b44fb5174e5b1a4ef6430d16d456",
+    "url": "/static/media/StephanieJacovine_cut.05a0b44f.png"
+  },
+  {
+    "revision": "a2ae0d1503774011992015345b78b5d7",
+    "url": "/static/media/Tadeu-franco.a2ae0d15.jpg"
+  },
+  {
+    "revision": "6369775124ced8d24db4470cbdd938f5",
+    "url": "/static/media/TropicalRomance.63697751.jpeg"
+  },
+  {
+    "revision": "f1166eab2f5d22a8a9c65f9d13427b34",
+    "url": "/static/media/assessoria.f1166eab.png"
+  },
+  {
+    "revision": "e2871d2b63300afd321972b0ad218098",
+    "url": "/static/media/desenvolvimento.e2871d2b.png"
+  },
+  {
+    "revision": "2a40238b9e0f8d5feb2d099660f7517f",
+    "url": "/static/media/distribui.2a40238b.png"
+  },
+  {
+    "revision": "2edbac05972ffff296b030c18e6faeb2",
+    "url": "/static/media/divulga.2edbac05.png"
+  },
+  {
+    "revision": "fe88e522361b3c4d53ce765c0ecf989a",
+    "url": "/static/media/img-george.fe88e522.png"
+  },
+  {
+    "revision": "7948c677241b9579a39ad3b17c9e9a37",
+    "url": "/static/media/img-sobre.7948c677.jpg"
+  },
+  {
+    "revision": "5c66bdfc164cd78c9778c03fd80a69d9",
+    "url": "/static/media/marketing.5c66bdfc.png"
+  },
+  {
+    "revision": "44c3d6d6511be15728adb74b1c704363",
+    "url": "/static/media/murilo-card.44c3d6d6.jpg"
+  },
+  {
+    "revision": "b28396865393d2e20f9bbcba4714b83a",
+    "url": "/static/media/murilo2.b2839686.png"
+  }
+]);
